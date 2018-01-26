29b39f005cb6b64d996f8a5727b293f04e138301
Decapitator1988 <30272324+Decapitator1988@users.noreply.github.com>
2018-01-26 22:33:38 +0200

    dsda

---
+++ src/main/java/com/decapitator/ExampleBean.java
@@ -0,0 +1,17 @@
+package com.decapitator;
+
+import javax.inject.Named;
+
+@Named
+public class ExampleBean {
+    private String text = "CDI Example";
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
+//
---
+++ src/main/java/com/decapitator/ExampleCDI.java
@@ -0,0 +1,4 @@
+package com.decapitator;
+
+public class ExampleCDI {
+}
